1660ce1063060ea3854ca6b71f30a7e49f83a665
jpb <jpb.06@outlook.com>
2019-07-02 20:35:41 +0200

    Api logic

---
+++ src/logic/file.system.ts
@@ -0,0 +1,62 @@
+﻿import * as fs from 'fs-extra';
+import * as crypto from 'crypto';
+import { Types } from 'dowpro-replay-watcher-dal';
+
+export async function readResult(
+    filePath: string
+): Promise<Types.GameResult | undefined> {
+
+    let raw: any = await fs.readJSON(filePath);
+
+    if (Types.validateGameResult(raw))
+        return raw as Types.GameResult;
+
+    return undefined;
+}
+
+export async function fileHash(
+    filePath: string,
+    algorithm = 'sha256'
+): Promise<string | undefined> {
+
+    return new Promise<string>(async (resolve, reject) => {
+        let shasum = crypto.createHash(algorithm);
+        try {
+            let stream = fs.createReadStream(filePath);
+            stream.on('data', (data) => {
+                shasum.update(data);
+            })
+            // making digest
+            stream.on('end', () => {
+                const hash = shasum.digest('hex');
+                return resolve(hash);
+            })
+        } catch (error) {
+            return reject(undefined);
+        }
+    });
+}
+
+export async function streamHash(
+    stream: NodeJS.ReadableStream,
+    algorithm = 'sha256'
+): Promise<string | undefined> {
+
+    return new Promise<string>((resolve, reject) => {
+        // Algorithm depends on availability of OpenSSL on platform
+        // Another algorithms: 'sha1', 'md5', 'sha256', 'sha512' ...
+        let shasum = crypto.createHash(algorithm);
+        try {
+            stream.on('data', (data) => {
+                shasum.update(data);
+            })
+            // making digest
+            stream.on('end', () => {
+                const hash = shasum.digest('hex');
+                return resolve(hash);
+            })
+        } catch (error) {
+            return reject(undefined);
+        }
+    });
+}
---
+++ src/logic/validation.ts
@@ -0,0 +1,28 @@
+﻿import { Types } from 'dowpro-replay-watcher-dal';
+import { MapData } from "relic-chunky-parser/typings/types/parsing.types";
+
+export function AreResultsMatching(
+    jsonResult: Types.GameResult,
+    relicChunkyResult: MapData
+): boolean {
+
+    let relicChunkyPlayers = relicChunkyResult.players.filter(el => el.race.length !== 0);
+
+    if (jsonResult.PlayersCount !== relicChunkyPlayers.length)
+        return false;
+
+    for (let i = 0; i < jsonResult.Players.length; i++) {
+
+        if (relicChunkyPlayers.find(el => el.name === jsonResult.Players[0].Name) === undefined)
+            return false;
+    }
+
+    let internalName = relicChunkyResult.internalName.substr(relicChunkyResult.internalName.lastIndexOf('\\') + 1);
+    if (internalName !== jsonResult.MapName)
+        return false;
+
+    if (Math.floor(relicChunkyResult.duration) !== jsonResult.Duration)
+        return false;
+
+    return true;
+}
