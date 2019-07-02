e599f714c6b109fb75a5c18440b5560f0d966c63
jpb <jpb.06@outlook.com>
2019-07-02 20:36:02 +0200

    Security route

---
+++ src/routes/security.routes.ts
@@ -0,0 +1,53 @@
+﻿import { Express, Request, Response } from "express-serve-static-core";
+import * as jwt from 'jsonwebtoken';
+import * as moment from 'moment';
+import * as Dal from 'dowpro-replay-watcher-dal';
+import { VaultService, Types } from 'rsa-vault';
+
+export function mapSecurityRoutes(app: Express) {
+
+    app.post('/api/login', async (
+        req: Request,
+        res: Response
+    ) => {
+        try {
+            if (!req.validateLogin()) {
+                return res.answer(400, 'Expecting identifiers');
+            }
+
+            let user = await Dal.Manipulation.AuthorizedUsersStore.get(req.body.login);
+            if (user === undefined) return res.status(401).json({
+                status: 401,
+                data: null
+            });
+
+            let isPasswordValid = await Dal.Util.Crypto.verify(req.body.password, user.password)
+            if (isPasswordValid) {
+                let applicationKeys: Types.ApplicationKeys = await VaultService.GetKeyPair('dowpro-ladder');
+
+                let gracePeriod = req.body.expiresIn || 120;
+                let expirationDate = moment().add(gracePeriod, 'seconds');
+
+                const jwtBearerToken = jwt.sign({ guild: req.body.login }, applicationKeys.privateKey, {
+                    algorithm: 'RS256',
+                    expiresIn: gracePeriod
+                });
+
+                return res.status(200).json({
+                    status: 200,
+                    token: jwtBearerToken,
+                    roles: user.roles,
+                    expirationDate: JSON.stringify(expirationDate)
+                });
+            } else {
+                return res.status(401).json({
+                    status: 401,
+                    data: null
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.answer(500, error.message);
+        }
+    });
+}
